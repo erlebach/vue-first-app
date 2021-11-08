f205544a6bfcac370768fa358aeee554d9cb4932
erlebach <>
2021-11-08 15:28:12 -0500

    Added text-processing.js to update ETA every x seconds.

---
+++ src/Composition/text-processing.js
@@ -0,0 +1,84 @@
+const API_URL = "http://35.223.143.175/api/dmhit";
+
+import { saveAs } from "file-saver";
+import { post } from "axios";
+import moment from "moment";
+
+const REQUEST_HEADERS = {
+  // POST
+  pwd: "M$h52NQV4_%N}mvc$w)-z*EuZ_^bf3",
+  arr_DTL: "2021-11-7",
+  days: 1,
+};
+
+const GetTableData = async () => {
+  let data = post(
+    "http://35.223.143.175/api/dmhit",
+    JSON.stringify({
+      pwd: "M$h`52NQV4_%N}mvc$w)-z$EuZ`_^bf3",
+      arr_DTL: moment()
+        .utc()
+        .subtract(24, "hours")
+        .format("YYYY-MM-DD"),
+      days: 1,
+    }),
+    {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+      return undefined;
+    });
+  return data;
+};
+
+function saveData() {
+  const keptRows = [];
+  GetTableData().then((response) => {
+    //console.log(response[0])
+    //console.log(response[1])
+    //console.log("Ignore all rows where both ETD and ETA are null (there are many)")
+    //console.log("ORIG_CD, DEST_CD, FLT_NUM, NEXT_FLT_NUM, SCH_DEP_DTMZ, SCH_ARR_DTMZ, ETD, ETA");
+    response[0].forEach((e) => {
+      if (e.ETA !== null) {
+        //console.log(`${e.ORIG_CD}, ${e.DEST_CD}, ${e.FLT_NUM}, ${e.SCH_DEP_DTMZ}, ${e.SCH_ARR_DTMZ}, ${e.ETD}, ${e.ETA}`);
+        const row = {
+          orig: e.ORIG_CD,
+          dest: e.DEST_CD,
+          fltnum: e.FLT_NUM,
+          sch_dep_dtmz: e.SCH_DEP_DTMZ,
+          sch_arr_dtmz: e.SCH_ARR_DTMZ,
+          etd: e.ETD,
+          eta: e.ETA,
+        };
+        keptRows.push(row);
+      }
+    });
+    //console.log("ORIG_CD, DEST_CD, FLT_NUM, SCH_DEP_DTMZ, SCH_ARR_DTMZ, ETD, ETA");
+    //console.log("Ignore all rows where both ETD and ETA are null (there are many)")
+  });
+  return keptRows;
+}
+
+const allUpdates = [];
+
+export function saveAtIntervals(nbSec) {
+  setInterval(() => {
+    const keptRows = saveData();
+    allUpdates.push(keptRows);
+    const now = moment().calendar();
+    console.log(`time: ${now}`);
+    console.log(allUpdates);
+    const filenm = "/data/flight_data_" + now + ".txt";
+    saveAs(new File(allUpdates, filenm, { type: "text/plain; charset=utf-8" }));
+    //console.log(filenm);
+    //console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+  }, nbSec * 1000); // ms
+}
---
+++ src/components/DelayPropagationTable.vue
@@ -0,0 +1,144 @@
+<template>
+  <!-- <div>
+    <DelayPropagationGraph
+      class="delay"
+      :width="600"
+      :height="1000"
+      v-show="false"
+    />
+  </div> -->
+
+  <div v-show="showTable">
+    <h3>
+      Propagated Delay Table, {{ nbRowsDelayed }} entries
+      <!-- <Button> Hide </Button> -->
+    </h3>
+    <DataTable
+      :value="delayedArrivalsTable"
+      :scrollable="true"
+      scrollHeight="300px"
+    >
+      <Column field="id" header="Flight ID" :sortable="true"> </Column>
+      <Column field="depDelay" header="depDelayZ" :sortable="true"> </Column>
+      <Column field="arrDelay" header="arrDelayZ" :sortable="true"> </Column>
+      <Column field="tail" header="tail" :sortable="true"></Column>
+      <Column field="level" header="level" :sortable="true"></Column>
+    </DataTable>
+  </div>
+</template>
+
+<script>
+import { computePropagationDelays } from "../Composition/propagationLibrary.js";
+// import DelayPropagationGraph from "./DelayPropGraph.vue";
+import * as io from "../Composition/IO_works";
+import { ref, watchEffect, watch } from "vue";
+import DataTable from "primevue/datatable";
+// import Button from "primevue/button";
+import Column from "primevue/column";
+import * as tier from "../Composition/Tierref";
+import { useStore } from "vuex";
+import { saveAtIntervals } from "../Composition/text-processing.js";
+
+const {
+  data1: dFSU,
+  data3: dTails,
+  data2: dBookings,
+  pend1,
+  pend2,
+  pend3,
+} = io.get3files(
+  "./data/node_attributes_daterange.json",
+  "./data/bookings_daterange.json",
+  "./data/tail_pairs_daterange.json" // Creates the problem (only when all three files used. WHY?)
+);
+
+export default {
+  components: { DataTable, Column },
+  props: {
+    filePath: String, // not used
+    tiers: String,
+  },
+  setup(props) {
+    const nbRowsDelayed = ref(0);
+    const showTable = ref(false);
+    const delayedArrivalsTable = ref(null);
+
+    const store = useStore();
+    tier.setTier(props.tiers);
+    const filesRead = ref(false);
+
+    // computed functions must be accessed by value (they are references)
+
+    watchEffect(() => {
+      // If I do not check all three conditions, even if not required, the contents will
+      // be executed possibly multiple times
+      if (
+        dTails.value &&
+        dFSU.value &&
+        dBookings.value &&
+        !pend1.value &&
+        !pend2.value &&
+        !pend3.value
+      ) {
+        filesRead.value = true; // NEW
+
+        saveAtIntervals(5); // save every 5 sec
+        // console.log("set ref.value to true");
+        store.commit("setFSU", dFSU);
+        store.commit("setTails", dTails);
+        store.commit("setBookings", dBookings);
+
+        // id should be chosen via interface
+        const initialID = "2019/10/01MIAPTY10:00173";
+
+        const table = computePropagationDelays(
+          dFSU,
+          dTails,
+          dBookings,
+          initialID
+        );
+
+        nbRowsDelayed.value = table.length;
+        delayedArrivalsTable.value = table;
+        showTable.value = true;
+      }
+    });
+
+    const rotStyle = (rotData) => {
+      const col = rotData < 45 ? "green" : "red";
+      return `color:${col}`;
+    };
+
+    return {
+      delayedArrivalsTable,
+      showTable,
+      nbRowsDelayed,
+      rotStyle,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.bold {
+  font-weight: bold;
+}
+
+.delay {
+  background-color: white;
+  border-color: green;
+}
+
+.p-datatable-table {
+  text-align: left;
+  color: red; /* not working */
+}
+
+p-datatable-wrapper {
+  text-align: left;
+  color: green; /* not working */
+}
+.p-column-title {
+  color: blue; /* worked  on all DataTables */
+}
+</style>
